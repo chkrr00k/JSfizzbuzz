b42a777e3511d740886b57738d2cda7c4d19fa7e
chkrr00k <trknnst132ff@gmail.com>
2018-03-06 23:29:46 +0100

    Initial commit

---
+++ LICENSE.md/fizzbuzz.js
@@ -0,0 +1,23 @@
+function fizzbuzz(max=100, min=1, cond={3 : "fizz", 5 : "buzz"}){
+    'use strict';
+    if(min >= max){
+        throw new Error("min >= max");
+    }
+    for(let i in cond){
+        if(i != parseInt(i, 10)){
+            throw new Error("Invalid json");
+        }
+    }
+    let chcond = (inp) => {
+        let result = "";
+        for(let tmp in cond){
+            if(!(inp % tmp)){
+                result += cond[tmp];
+            }
+        }
+        return result||inp;
+    };
+    for(let i = min; i < max; i++){
+        console.log(chcond(i));
+    }
+}
